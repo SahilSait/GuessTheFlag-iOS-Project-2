a5a445af08dfbd2bb559e30528e1bd98b0c85604
Sahil Sait <sahilsaitdev@gmail.com>
2021-07-29 23:07:24 +0530

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Sahil Sait on 29/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack() {
+            Color.red.ignoresSafeArea()
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sahil Sait on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
